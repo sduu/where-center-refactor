39450d150ee2e7b8e493ab7a2b676bb0a9c73769
sduu <qjqmfrja00@gmail.com>
2023-06-05 14:03:12 +0900

    Update : Router 내 lazy load와 Suspense 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainPage from '@/pages/mainPage/MainPage';
-import MapPage from '@/pages/mapPage/MapPage';
-import NotFoundPage from '@/pages/notFoundPage/NotFoundPage';
-import SplashPage from '@/pages/splashPage/SplashPage';
-import Layout from '@/components/layout/Layout';
+import Spinner from '../components/common/spinner/Spinner';
+import Layout from '../components/layout/Layout';
+
+const MainPage = lazy(() => import('../pages/mainPage/MainPage'));
+const MapPage = lazy(() => import('../pages/mapPage/MapPage'));
+const NotFoundPage = lazy(() => import('../pages/notFoundPage/NotFoundPage'));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<SplashPage />} />
-          <Route path='/main' element={<MainPage />} />
-          <Route path='/map' element={<MapPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<MainPage />} />
+            <Route path='/map' element={<MapPage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
