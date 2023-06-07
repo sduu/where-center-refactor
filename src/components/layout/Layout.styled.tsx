414366f58f5fffcafc7d7b00bebc9fbbfa2365c1
sduu <qjqmfrja00@gmail.com>
2023-06-07 15:57:58 +0900

    Add : 공통 Layout 스타일링

--- src/components/layout/Layout.styled.tsx
+++ src/components/layout/Layout.styled.tsx
@@ -1,3 +1,24 @@
 import styled from 'styled-components';
+import { BORDER_RADIUS, BREAKPOINT, COLOR, GAP } from '../../constants/styles.constant';
+import { smLayout } from '../../styles/GlobalMixin';
 
-export const LayoutWrapper = styled.div``;
+export const LayoutWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: ${COLOR.background};
+
+  .main-wrapper {
+    max-width: ${BREAKPOINT.sm};
+    width: 100%;
+    min-height: 70vh;
+    padding: ${GAP.md};
+    border-radius: ${BORDER_RADIUS.md};
+    background-color: ${COLOR.backgroundSection};
+
+    ${smLayout} {
+      min-height: 100vh;
+    }
+  }
+`;
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import { LayoutWrapper } from './Layout.styled';
 
 const Layout = () => {
-  return <LayoutWrapper>Layout</LayoutWrapper>;
+  return (
+    <LayoutWrapper>
+      <main className='main-wrapper'>
+        <Outlet />
+      </main>
+    </LayoutWrapper>
+  );
 };
 
 export default Layout;
