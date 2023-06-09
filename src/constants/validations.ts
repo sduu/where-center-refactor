5263166a3ed92e49c507c23a757f69747a55c79d
sduu <qjqmfrja00@gmail.com>
2023-06-09 17:55:04 +0900

    Update : validations 조건 업데이트

--- src/constants/validations.ts
+++ src/constants/validations.ts
@@ -3,17 +3,27 @@ interface ValidationRules {
   message: string;
 }
 
-export const validations: Record<string, ValidationRules> = {
-  name: {
-    condition: (value: string) => value.length > 0,
-    message: '이름을 입력해주세요.',
-  },
-  email: {
-    condition: (value: string) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value),
-    message: '유효한 이메일 주소를 입력해주세요.',
-  },
-  password: {
-    condition: (value: string) => value.length >= 8,
-    message: '비밀번호는 8자 이상이어야 합니다.',
-  },
+export const validations: Record<string, ValidationRules[]> = {
+  name: [
+    {
+      condition: (value: string) => value.length > 0,
+      message: '이름을 입력해주세요.',
+    },
+    {
+      condition: (value: string) => /^[a-zA-Z0-9]{3,20}$/i.test(value),
+      message: '유저 이름은 영문 대소문자와 숫자로만 구성되어야 하며, 최소 3자 이상 20자 이하여야 합니다.',
+    },
+  ],
+  email: [
+    {
+      condition: (value: string) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value),
+      message: '유효한 이메일 주소를 입력해주세요.',
+    },
+  ],
+  address: [
+    {
+      condition: (value: string) => value.length > 0,
+      message: '주소를 입력해주세요.',
+    },
+  ],
 };
